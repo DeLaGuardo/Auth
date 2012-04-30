402783865567e8e98a7aad90e8b000f872def09a
DeLaGuardo <delaguardo@gmail.com>
2012-04-30 20:03:25 +0400

    init commit

---
+++ Auth4/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth4::Application.config.secret_token = '560a53957e7032bcf3f26b226a94461b7cbf7f03b05ac653955f7c52b2c121b5b120a9f2600a9b0552d232563271ad2d25cf6fdf1717e390b367b7bc1661572e'
